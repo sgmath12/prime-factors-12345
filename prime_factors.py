7d9a89d9117f91ea8c3ad1337c4ffcbafc09ac7d
sgmath12 <sgmath12@gmail.com>
2026-07-01 13:04:30 +0900

    [refactoring] #47

--- prime_factors.py
+++ prime_factors.py
@@ -7,8 +7,12 @@ class PrimeFactor:
                     factors.append(2)
                     number //= 2
             elif number == 6 :
-                factors.append(2)
-                factors.append(3)
+                divisor = 2
+                while number > 1 :
+                    while number % divisor == 0 :
+                        factors.append(divisor)
+                        number //= divisor
+                    divisor += 1
             else:
                 factors.append(number)
         return factors
